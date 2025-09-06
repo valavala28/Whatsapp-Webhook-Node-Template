873065634dbfe443c624ad042262bfdffbc95e15
Rajeswari valavala <128347437+valavala28@users.noreply.github.com>
2025-09-06 12:16:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2341,7 +2341,7 @@ app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));*/
 
 
 
-/*const express = require("express");
+const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
@@ -2356,13 +2356,13 @@ const PHONE_ID = "749224044936223";
 const TOKEN = "EAARCCltZBVSgBPJQYNQUkuVrUfVt0rjtNIaZBNVO7C24ZC5b5RO4DJKQOVZC5NWSeiknzZBrDec88QkAYYji7ypvDBgL1GDw3E39upO2TbuW8IfGx94VuH7bJpFKngdyJOjexp6SN6wYEM0Ah6MOERatzhjeth0sHeo8GneT6kyXyaPyHZA94Exe9NKVJZBIisrxAZDZD";
 const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyKwi4iXPptEb3uJuOcybGf41_zYu69VqPmDYNh8qi1RyMJfv2isgxaZfHh788Cfka78g/exec";
 
-// -------------------- Send Template proactively --------------------
+/* -------------------- Send Template proactively --------------------
 async function sendTemplate(to, name = "Customer") {
   try {
     const headers = { Authorization: `Bearer ${TOKEN}` };
 
     const response = await axios.post(
-      `https://graph.facebook.com/v21.0/${PHONE_ID}/messages`,
+      `https://graph.facebook.com/v23.0/${PHONE_ID}/messages`,
       {
         messaging_product: "whatsapp",
         to,
@@ -2389,7 +2389,7 @@ async function sendTemplate(to, name = "Customer") {
   } catch (err) {
     console.error("❌ Failed to send template:", err.response?.data || err.message);
   }
-}*
+}*/
 
 //-------------------- Send Template proactively --------------------
 async function sendTemplate(to, name = "Customer") {
@@ -2778,10 +2778,10 @@ app.post("/webhook", async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));*/
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
 
-const express = require("express");
+/*const express = require("express");
 const bodyParser = require("body-parser");
 const axios = require("axios");
 
@@ -3102,5 +3102,5 @@ app.post("/webhook", async (req, res) => {
 });
 
 // Start server
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));*/
 
