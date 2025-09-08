697e45ea21752f8dd02ce8512d363dd7b18ba99d
Rajeswari valavala <128347437+valavala28@users.noreply.github.com>
2025-09-08 12:20:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const axios = require("axios");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
